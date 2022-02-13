0515540a8e2a4f7a2c6becfc3988e9b0ce1c1155
ProjectWeater <97742494+u2t-saopao@users.noreply.github.com>
2022-02-13 22:23:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -79,7 +79,21 @@
 
             <div class="container">
                 <div class="row">
-                    
+                <div class="card">
+                    <div class="card-wind-speed" id="wind-speed">
+                        <span>Temperature</span>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-wind-speed1" id="wind-speed1">
+                        <span>Humidity</span>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-wind-vane" id="wind-vane">
+                        <span>LED</span>
+                    </div>
+                </div>
                 </div>
             </div>
             
