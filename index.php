e17c773857417d51d7c3b61924d323dc87ce628b
ProjectWeater <97742494+u2t-saopao@users.noreply.github.com>
2022-02-13 22:19:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <title>Contact | Agency Perfect</title>
+    <title>Weather | Station</title>
 
     <meta name="description" content="">
     <meta name="author" content="">
@@ -79,62 +79,12 @@
 
             <div class="container">
                 <div class="row">
-                    <div class="col-sm-7">
-                        <h2>Send a message</h2>
-                        <form>
-                            <div class="row">
-                                <div class="col-sm-6">
-                                    <div class="form-group">
-                                      <label for="name">Name:</label>
-                                      <input type="text" class="form-control" id="name">
-                                    </div>
-                                </div>
-                                <div class="col-sm-6">
-                                    <div class="form-group">
-                                      <label for="email">E-mail:</label>
-                                      <input type="email" class="form-control" id="email">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                              <label for="message">Subject:</label>
-                              <input class="form-control" id="subject"></input>
-                            </div>
-                            <div class="form-group">
-                              <label for="message">Message:</label>
-                              <textarea class="form-control form-control-comment" id="message"></textarea>
-                            </div>
-                            <div class="text-center">
-                                <button class="btn btn-green">Contact us</button>
-                            </div>
-                        </form>
-                    </div>
-                    <div class="col-sm-5">
-                        <div class="contact-info">
-                            <h2>Contact information</h2>
-                            <div class="row">
-                                <div class="col-sm-12">
-                                    <h3>Address</h3>
-                                    <ul class="list-unstyled">
-                                        <li>Sydney, Australia</li>
-                                        <li>100 Mainstreet Center</li>
-                                    </ul>
-                                    <h3>E-mail</h3>
-                                    <a href="mailto:pixelperfectmk@gmail.com" target="_blank">pixelperfectmk@gmail.com</a>
-                                    <h3>Phone</h3>
-                                    <a href="tel:2083339296" target="_blank">(208) 333 9296</a>
-                                    <h3>Fax</h3>
-                                    <p>(208) 333 9296</p>
-                                </div>
-                            </div>
-                        </div><!-- /.contact-info -->
-                    </div>
+                    
                 </div>
             </div>
             
         </section><!-- /.section-contact-us -->
 
-        <section id="map" class="section-map"></section><!-- /.section-map -->
 
     </main><!-- /#main -->
 
